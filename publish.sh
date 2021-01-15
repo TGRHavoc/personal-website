2a98b419d20d9fbfdb0bfdcc87cd3e7379c501f1
Jordan Dalton <jordan.8474@gmail.com>
2021-01-15 16:20:35 +0000

    feat: add website and matrix social

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Build the site files. If Github API token needed, add here
-JEYKLL_GITHUB_TOKEN=""
-bundle exec jekyll build
-
-#Copy the files onto the server
-scp -r _site/* tgrhavoc.me:/sharedfolders/tgrhavoc.me/
-
